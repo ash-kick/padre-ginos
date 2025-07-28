f627097e54224c0fa3fed8e76066131284750712
ash-kick <ashley.kicklighter@gmail.com>
2025-07-28 11:20:19 -0700

    Deploying code and updating vite and package configs

--- vite.config.js
+++ vite.config.js
@@ -3,24 +3,25 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-      "/public": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
-  plugins: [
-    TanStackRouterVite(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
-      },
-    }),
-  ],
+     server: {
+          proxy: {
+               "/api": {
+                    target: "http://localhost:3000",
+                    changeOrigin: true,
+               },
+               "/public": {
+                    target: "http://localhost:3000",
+                    changeOrigin: true,
+               },
+          },
+     },
+     base: "/padre-ginos/",
+     plugins: [
+          TanStackRouterVite(),
+          react({
+               babel: {
+                    plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+               },
+          }),
+     ],
 });
